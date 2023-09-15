8a4ba7f16cae8f583d77530f5ab75b5816f52aea
Julien de Castelnau <julien.decastelnau@epfl.ch>
2023-09-15 18:25:11 +0200

    Testing out old comms

--- include/zenith/comms/n64.h
+++ include/zenith/comms/n64.h
@@ -37,6 +37,40 @@ typedef struct {
     int8_t stick_y;
 } __attribute__((packed)) n64_input_t;
 
+enum N64Command {
+    PROBE = 0x00,
+    RESET = 0xFF,
+    POLL = 0x01,
+    READ_EXPANSION_BUS = 0x02,
+    WRITE_EXPANSION_BUS = 0x03,
+};
+
+typedef struct __attribute__((packed)) {
+    uint16_t device;
+    uint8_t status;
+} n64_status_t;
+
+static const n64_status_t default_n64_status = {
+    .device = 0x0005,
+    .status = 0x02,
+};
+
+static const n64_status_t other_status = {
+    .device = 0xFFFF,
+    .status = 0xFF,
+};
+
+/**
+ * @brief A structure representing a Joybus instance on a given GPIO pin
+ */
+typedef struct {
+    uint pin;
+    PIO pio;
+    uint sm;
+    uint offset;
+    pio_sm_config config;
+} joybus_port_t;
+
 void n64_init();
 
 void n64_comms_task(uint32_t timestamp, btn_data_t *buttons,
